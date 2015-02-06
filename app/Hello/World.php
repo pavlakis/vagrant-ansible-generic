66e26b0d8b4672a45b7757a29fa27506c6c405db
Antonios Pavlakis <antonis@pavlakis.info>
2015-02-06 14:02:14 +0000

    [Application] A basic script using composer's autoloader with a namespace as a basic example.

---
+++ app/Hello/World.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Antonios Pavlakis <antonios@pavlakis.info>
+ * @copyright 2015 Antonios Pavlakis
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+
+namespace MySite\Hello;
+
+
+class World
+{
+
+    public function __toString()
+    {
+        return 'Hello brave new World with Vagrant and Ansible!!!';
+    }
+}
